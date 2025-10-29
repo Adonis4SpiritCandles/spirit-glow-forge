5131f32d35639aaf89c0492afcb07047fb690978
Adonis4SpiritCandles <giacomofurlari@libero.it>
2025-10-29 16:17:06 +0000

    edit upload mod prova VEDIAMO!

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,10 @@
-import React from "react";
-import { createRoot } from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
-import App from "./App.tsx";
-import "./index.css";
+import { createRoot } from 'react-dom/client'
+import App from './App.tsx'
+import './index.css'
+import { HelmetProvider } from 'react-helmet-async';
 
 createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
-    <BrowserRouter basename="/spiritcandles">
-      <App />
-    </BrowserRouter>
-  </React.StrictMode>
+  <HelmetProvider>
+    <App />
+  </HelmetProvider>
 );
